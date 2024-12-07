50d421b72cbbd43cd17779688c8a61aa8e1ec989
Mikel_Bracamonte <mikel.bracamonte@utec.edu.pe>
2024-12-07 18:32:21 -0500

    arregle cositas

--- app.js
+++ app.js
@@ -7,11 +7,18 @@ console.log(`Servidor WebSocket escuchando en el puerto ${port}`);
 
 server.on('connection', (socket) => {
   console.log('Nuevo cliente conectado');
+  console.log(socket);
+  let playerUUID;
 
   // Recibir mensajes de los clientes
   socket.on('message', (message) => {
     console.log(`Mensaje recibido: ${message}`);
     const parsedMessage = JSON.parse(message);
+
+    if(parsedMessage.type == "playerUpdate") {
+      playerUUID = parsedMessage.playerUUID;
+    }
+
     // Convertir el mensaje a texto antes de enviarlo a otros clientes
     const messageToSend = JSON.stringify(parsedMessage);
 
@@ -25,5 +32,12 @@ server.on('connection', (socket) => {
   // Manejar la desconexión del cliente
   socket.on('close', () => {
     console.log('Cliente desconectado');
+    const type = "close";
+    const closeData = JSON.stringify({type, playerUUID});
+    server.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(closeData); // Asegurar que el mensaje sea texto
+      }
+    });
   });
 });
